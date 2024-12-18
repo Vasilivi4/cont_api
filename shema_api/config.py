f5d6c1911ef6494d7744fc30c9ebeb880338348f
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2024-12-18 09:03:56 +0200

    Create config.py

---
+++ shema_api/config.py
@@ -0,0 +1,23 @@
+"""Module providing a function printing python version."""
+
+import os
+from pathlib import Path
+from fastapi_mail import ConnectionConfig
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")
+
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT")),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_FROM_NAME=os.getenv("MAIL_FROM_NAME"),
+    MAIL_STARTTLS=False,
+    MAIL_SSL_TLS=True,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(__file__).parent / 'temp',
+)
